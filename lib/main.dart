25b1b0abc0c2a6541c9972fbfa185e90d0f1b2cf
Tharindu Akalanka <tharinduhccakalanka@gmail.com>
2025-12-09 00:56:28 +0530

    added ned files codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:student_registration_app/firebase_options.dart';
 import 'package:student_registration_app/screens/splash/splash.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
