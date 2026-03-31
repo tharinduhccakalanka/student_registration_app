7dbe8337c630651e6a2329b41bc102ba30afe46c
Tharindu Akalanka <tharinduhccakalanka@gmail.com>
2026-03-31 23:51:51 +0530

    updated codes

---
+++ lib/trail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Trail extends StatefulWidget {
+  const Trail({super.key});
+
+  @override
+  State<Trail> createState() => _TrailState();
+}
+
+class _TrailState extends State<Trail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
